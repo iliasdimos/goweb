9eceb0a0dce9458d317483fba97244ee3e9989a2
Ilias Dimos <iliasdimos@gmail.com>
2018-04-10 22:16:15 +0300

    add handling for sigint and version

--- main.go
+++ main.go
@@ -4,13 +4,30 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
+
+var version = "v1"
 
 func main() {
 	port := ":" + getPort()
 
+	// Handling signals
+	signals := make(chan os.Signal)
+	done := make(chan bool)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go catchSignal(signals, done)
+	// End of handling signals
+
 	http.HandleFunc("/", indexHandler)
+	fmt.Printf("Starting server at %s\n", port)
+	fmt.Println("Press ctrl-c to terminate...")
 	http.ListenAndServe(port, nil)
+
+	<-done
+	fmt.Println("Closing now ! Bye !")
+	os.Exit(1)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +35,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Fprintf(w, "<!DOCTYPE html><html><h2>Simple Go Web Server</h2><body style='background-color: %s'> Running on %s</body></html>", getColor(), host)
+	fmt.Fprintf(w, "<!DOCTYPE html><html><h2>Simple Go Web Server %s</h2><body style='background-color: %s'> Running on %s</body></html>", version, getColor(), host)
 
 }
 
@@ -36,4 +53,21 @@ func getColor() string {
 		return "white"
 	}
 	return color
+}
+
+func catchSignal(ch chan os.Signal, done chan bool) {
+	sig := <-ch
+
+	fmt.Println("sig received:", sig)
+
+	switch sig {
+	case syscall.SIGINT:
+		fmt.Println("Handling a SIGINT here")
+	case syscall.SIGTERM:
+		fmt.Println("Handling a SIGTERM here")
+	default:
+		fmt.Println("Other signal received")
+	}
+
+	done <- true
 }
